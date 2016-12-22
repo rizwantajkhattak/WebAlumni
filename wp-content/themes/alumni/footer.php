d55e931d5d2d9bff6b6cce3db3b71d7ee0f9e591
rizwantaj <rizwantajkhattak@hotmail.com>
2016-12-22 13:53:55 +0800

    Updating HTML file to Dynamic files

---
+++ wp-content/themes/alumni/footer.php
@@ -0,0 +1,55 @@
+<!--Footer Part Start-->
+          <footer id="footer">
+            <div class="fpart-inner">
+
+              <div class="column">
+                <h3>Information</h3>
+                <ul>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">Site Map</a></li>
+                  <li><a href="#">Privacy & Accessibility</a></li>
+                </ul>
+              </div>
+
+              <!-- Contact Details Start-->
+              <div class="contact contact_icon">
+                <h3>Contact Us</h3>
+                <ul>
+                  <li class="address">Chonqqing, ShapingBa, China PRC</li>
+                  <li class="mobile"><a href="tel:+861352759895">+86 13527598954</a></li>
+                  <li class="email"><a href="mailto:feilong.dragona@gmail.com?Subject=From CQU Alumni WebSite">feilong.dragona</a></li>
+                </ul>
+              </div>
+              <!-- Contact Details End-->
+              <!-- Contact Social Start-->
+              <div class="contact contact_icon">
+                <h3>Our Social Links</h3>
+                <div class="payments_types part3"> <img src="image/chongda.png" alt="Chongda" title="Chongqing University Website"> <img src="image/cqu_news.png" alt="CQU News" title="CQU News">  <img src="image/weibo.png" alt="Weibo CQU" title="Weibo account of Chongqing University"> <img src="image/wechat.png" alt="Wechat" title="Wechat CQU"> <img src="image/qq.png" alt="QQ CQU" title="QQ CQU"> </div>
+              </div>
+              <!-- Contact Social End-->
+              
+              
+              <div class="clear"></div>
+              <div id="powered">
+                <!-- Payment Images Icon Start-->
+                
+                <!-- Payment Images Icon End-->
+                <!-- Powered by Text Start-->
+                <div class="powered_text part3">
+                  <p>For all Discipulus of CQU ©  2016 - 2017 <br />
+                    Template By <a target="_blank" href="#">Feilong</a></p>
+                  </div>
+                  <!-- Powered by Text End-->
+
+                  <div class="clear"></div>
+                </div>
+                <!-- Back to Top Button Start-->
+                <div class="back-to-top" id="back-top"><a title="Back to Top" href="javascript:void(0)" class="backtotop">Top</a></div>
+                <!-- Back to Top Button End-->
+              </div>
+            </footer>
+            <!--Footer Part End-->
+          </div>
+        </body>
+        </html>
---
+++ wp-content/themes/alumni/index.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+        <div id="container">
+
+          <!--Middle Part Start-->
+          <div id="content">
+
+            <h1 class="add-margin-cqu-color-dark">Welcome to Chongqing University Student Alumni association!  </h1>
+
+
+
+            <div class="login-content">
+              <div class="left">
+                <p><strong>Dear CQU alumni,</strong> <br/>
+
+                  Greetings from the School of International Education, Chongqing University (CQU)!
+
+                  How time flies! It’s been some time since you graduated from CQU. We hope you are all doing exceptionally well in your fields. As you have spent some wonderful time in China and at CQU specifically, we believe that you will serve as a bridge between China and your home country and play an important role in international friendship. We hope, therefore, to keep in touch with you on a regular basis. So, if you change your contact information, please kindly update it by completing the form attached and sending it to us at internationalalumni@cqu.edu.cn.</p>
+                </div>
+                <div class="right">
+
+                  <h2>Are you a CQU student or an Alumni?</h2>
+                  <div class="content">
+                    <p>By creating an account you will be able to get in touch with all the other alumnies of CQU.</p>
+                    <a class="button" href="#">Join now</a>
+                  </div>
+
+                </div>
+              </div>
+              <h1 class="h1-cqu-color-dark">Up coming events</h1>
+              <p>
+                Greetings from the School of International Education, Chongqing University (CQU)!
+
+                How time flies! It’s been some time since you graduated from CQU. We hope you are all doing exceptionally well in your fields. As you have spent some wonderful time in China and at CQU specifically, we believe that you will serve as a bridge between China and your home country and play an important role in international friendship. We hope, therefore, to keep in touch with you on a regular basis. So, if you change your contact information, please kindly update it by completing the form attached and sending it to us at internationalalumni@cqu.edu.cn.</p>
+              </div>
+              <!--Middle Part End-->
+              <div class="clear"></div>
+            </div>
+          </div>
+
+<?php get_footer(); ?>          
